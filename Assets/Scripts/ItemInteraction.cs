28cc78088c45594180b6cd94f617e38c65c6681e
rageCode153 <ChrisKonstantopoulos2002@gmail.com>
2021-03-06 16:49:52 +0200

    Created new interaction script for notes and general use

---
+++ Assets/Scripts/ItemInteraction.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class ItemInteraction : MonoBehaviour
+{
+    public bool isInRange=false;
+    public bool isOpen;
+    public KeyCode interactKey;
+    public UnityEvent interactActionOpen;
+    public UnityEvent interactActionClose;
+
+    private void Update()
+    {
+        if(isInRange)
+        {
+            if(Input.GetKeyDown(interactKey))
+            {
+                if (isOpen)
+                {
+                    interactActionClose.Invoke();
+                    isOpen = false;
+                }
+                else
+                {
+                    interactActionOpen.Invoke();
+                    isOpen = true;
+                }
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+}
---
+++ Assets/Scripts/Note.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Note : MonoBehaviour
+{
+    public Image noteImage;
+    public KeyCode interactKey;
+    public bool isOpen;
+    public bool isInRange;
+
+    private void Update()
+    {
+        if(isInRange && Input.GetKeyDown(interactKey))
+        {
+            if(isOpen)
+            {
+                CloseNote();
+                isOpen = false;
+            }
+            else
+            {
+                OpenNote();
+                isOpen = true;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            isInRange = true;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isInRange = false;
+        }
+    }
+
+    public void OpenNote()
+    {
+        noteImage.enabled = true;
+    }
+
+    public void CloseNote()
+    {
+        noteImage.enabled = false;
+    }
+}
